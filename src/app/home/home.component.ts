dad80a88d68ec4b5d68c082fee8bf1844749c5e9
Resdan Logan <rezn5447@gmail.com>
2018-02-11 23:52:51 -0800

    confusing animation

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -15,21 +15,27 @@ import {
   styleUrls: ['./home.component.scss'],
   animations: [
     trigger('goals', [
-      transition(
-        '* => *', [
+      transition('* => *', [
         query(':enter', style({ opacity: 0 }), { optional: true }),
         query(
           ':enter',
           stagger('300ms', [
             animate(
               '.6s ease-in',
-              keyframes([style({ opacity: 0, transform: 'translateY(-75%)', offset: 0 }),
-              keyframes([style({ opacity: .5, transform: 'translateY(-75%)', offset: .3 }),
-              keyframes([style({ opacity: 0, transform: 'translateY(-75%)', offset: 1 })
-            ]))
-          ])
-        ])
-      )
+              keyframes([
+                style({ opacity: 0, transform: 'translateY(-75%)', offset: 0 }),
+                style({
+                  opacity: 0.5,
+                  transform: 'translateY(35px)',
+                  offset: 0.3
+                }),
+                style({ opacity: 1, transform: 'translateY(0)', offset: 1 })
+              ])
+            )
+          ]),
+          { optional: true }
+        )
+      ])
     ])
   ]
 })
